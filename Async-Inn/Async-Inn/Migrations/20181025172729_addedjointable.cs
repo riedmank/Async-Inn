9b9b8ecf3a6f8e6bec50cef1c84b0e025f65d9be
Kristofer Riedman <riedmank@hotmail.com>
2018-10-25 10:53:59 -0700

    built tables adding scaffolding

---
+++ Async-Inn/Async-Inn/Migrations/20181025172729_addedjointable.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class addedjointable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Room",
+                table: "Room");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Hotel",
+                table: "Hotel");
+
+            migrationBuilder.RenameTable(
+                name: "Room",
+                newName: "Rooms");
+
+            migrationBuilder.RenameTable(
+                name: "Hotel",
+                newName: "Hotels");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Rooms",
+                table: "Rooms",
+                column: "RoomID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Hotels",
+                table: "Hotels",
+                column: "HotelID");
+
+            migrationBuilder.CreateTable(
+                name: "HotelRooms",
+                columns: table => new
+                {
+                    RoomNumber = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HotelID = table.Column<int>(nullable: false),
+                    RoomID = table.Column<int>(nullable: false),
+                    Rate = table.Column<decimal>(nullable: false),
+                    PetFriendly = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelRooms", x => x.RoomNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomAmenities",
+                columns: table => new
+                {
+                    AmenitiesID = table.Column<int>(nullable: false),
+                    RoomID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomAmenities", x => new { x.AmenitiesID, x.RoomID });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelRooms");
+
+            migrationBuilder.DropTable(
+                name: "RoomAmenities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Rooms",
+                table: "Rooms");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Hotels",
+                table: "Hotels");
+
+            migrationBuilder.RenameTable(
+                name: "Rooms",
+                newName: "Room");
+
+            migrationBuilder.RenameTable(
+                name: "Hotels",
+                newName: "Hotel");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Room",
+                table: "Room",
+                column: "RoomID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Hotel",
+                table: "Hotel",
+                column: "HotelID");
+        }
+    }
+}
