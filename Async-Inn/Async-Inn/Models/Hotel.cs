a0d05d3ac9b237c8b89e6aaeaf223f0655625a67
Kristofer Riedman <riedmank@hotmail.com>
2018-10-25 16:37:10 -0700

    refined code and added styles to all pages

--- Async-Inn/Async-Inn/Models/Hotel.cs
+++ Async-Inn/Async-Inn/Models/Hotel.cs
@@ -11,5 +11,7 @@ namespace AsyncInn.Models
         public string Name { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
+
+        public ICollection<HotelRoom> HotelRooms { get; set; }
     }
 }
